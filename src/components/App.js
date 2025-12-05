4d55d1dec201ed25ee65b695eb8052f153b47ab2
Yash <yashmahalle33@gmail.com>
2025-12-05 13:16:11 +0530

    first commit

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,21 @@
-
-import React from "react";
-import './../styles/App.css';
-
-const App = () => {
-  return (
-    <div>
-        {/* Do not remove the main div */}
-    </div>
-  )
+import React, { useEffect, useState } from "react"
+export default function SumCalculator ()
+{
+    const[sum,setSum] = useState(0);
+    const[inputNum,setInputNum] = useState(0);
+    console.log(inputNum);
+    useEffect(()=>{
+        setSum(prev=>prev+Number(inputNum));
+    },[inputNum])
+    return(
+       
+       <>
+       <h1>Sum Calculator</h1>
+       <input type="number" onChange={(e)=>{
+        setInputNum(e.target.value);
+       }}/>
+        <p>Sum: {sum}</p>
+       </>
+       
+    )
 }
-
-export default App
